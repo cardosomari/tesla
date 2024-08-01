6bc31455ba13812ae17a60fb2436b5a1af288375
cardosomari <marianamora.1bmangal@gmail.com>
2024-08-01 18:50:11 +0000

    Projeto Tesla

---
+++ script.js
@@ -0,0 +1,10 @@
+let formulario =
+    document.querySelector(".formulario");
+//função para aparecer o form
+function aparecerFormulario(){
+    formulario.style.left="50%";
+}
+//função para desparecer o form
+function desparecerFormulario(){
+    formulario.style.left="-50%";
+}
